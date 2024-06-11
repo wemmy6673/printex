95cc1075b67752b11a4fb86ddd507dc49a522f3b
wemmy6673 <okunolawemzy@gmail.com>
2024-06-11 12:50:02 +0100

    External array of objects created

---
+++ src/components/Items.js
@@ -0,0 +1,68 @@
+import Cap from '../images/Cap.png';
+
+let Items = [{
+    id: 1,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 2,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 3,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 4,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 5,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 6,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 7,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 8,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+{
+    id: 9,
+    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
+    new_price: 200,
+    old_price: 250,
+    image: Cap,
+},
+]
+
+export default Items;
--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -2,73 +2,10 @@ import React from 'react';
 import { BsSearch } from "react-icons/bs";
 import { BsHeartFill } from "react-icons/bs";
 import {BsHeart} from "react-icons/bs";
-import Cap from '../images/Cap.png';
+import Items from './Items';
 import { useState } from 'react';
 
-const Items = [{
-    id: 1,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 2,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 3,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 4,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 5,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 6,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 7,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 8,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-{
-    id: 9,
-    title: "Under Armour Training stretch Woven Half Zip Jacket In White",
-    new_price: 200,
-    old_price: 250,
-    image: Cap,
-},
-]
+
 
 
 const ProductDisplay = () => {
